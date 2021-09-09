30b7cab1e7e1bf3653f31a7cd1d51205d9b6a842
blargg <tomjankauski@gmail.com>
2021-09-08 23:39:16 -0700

    Format schulze

--- src/schulze_method.rs
+++ src/schulze_method.rs
@@ -1,4 +1,4 @@
-use crate::{Candidate, Ballot, BallotSlice, unique_candidates, PairwisePreferences};
+use crate::{unique_candidates, Ballot, BallotSlice, Candidate, PairwisePreferences};
 
 /// Schulze method election. Ballots give a list of candidates and a number ranking.
 /// Lower numbers are more preferred candidates. Each candidate can only be listed
@@ -19,9 +19,7 @@ pub fn schulze_method(votes: Vec<Ballot>) -> Vec<Candidate> {
     let count = PairwisePreferences::from_ballots(&votes);
     let widest_paths = floyd_warshall_widest_paths(&count.counts);
 
-    let mut candidates_to_sort = count
-        .candidates()
-        .collect::<Vec<_>>();
+    let mut candidates_to_sort = count.candidates().collect::<Vec<_>>();
     candidates_to_sort.sort_by_key(|candidate| preferred_above_count(&widest_paths, *candidate));
     candidates_to_sort.reverse();
     candidates_to_sort
@@ -29,7 +27,9 @@ pub fn schulze_method(votes: Vec<Ballot>) -> Vec<Candidate> {
 
 /// schulze_method, but only returns the first candidate.
 pub fn schulze_method_single(votes: Vec<Ballot>) -> Candidate {
-    *schulze_method(votes).get(0).expect("Expecting there to be at least one candidate.")
+    *schulze_method(votes)
+        .get(0)
+        .expect("Expecting there to be at least one candidate.")
 }
 
 // Checks if the ballot is valid.
@@ -72,7 +72,9 @@ fn floyd_warshall_widest_paths(weights: &[Vec<i32>]) -> Vec<Vec<i32>> {
 fn preferred_above_count(preferences: &[Vec<i32>], candidate: Candidate) -> usize {
     let mut count = 0;
     for other in 0..preferences.len() {
-        if candidate.id == other { continue; }
+        if candidate.id == other {
+            continue;
+        }
         if preferences[candidate.id][other] >= preferences[other][candidate.id] {
             count += 1;
         }
@@ -114,19 +116,23 @@ mod test {
         length.prop_flat_map(|length| vec(vec(0..100, length), length))
     }
 
-    fn shuffled_subsequence(values: std::ops::Range<usize>, size: usize) -> impl Strategy<Value = Vec<usize>> {
+    fn shuffled_subsequence(
+        values: std::ops::Range<usize>,
+        size: usize,
+    ) -> impl Strategy<Value = Vec<usize>> {
         let values = values.collect::<Vec<_>>();
         use proptest::sample::subsequence;
         subsequence(values, size).prop_shuffle()
     }
 
-    fn weights_and_path() -> impl Strategy<Value=(Vec<Vec<i32>>, Vec<usize>)>{
+    fn weights_and_path() -> impl Strategy<Value = (Vec<Vec<i32>>, Vec<usize>)> {
         // So far, this is only used in props that make sense with 2 or more length
         let length = 2..10usize;
         length.prop_flat_map(|num_vertecies| {
             (1..num_vertecies).prop_flat_map(move |path_length| {
                 square_vec(Just(num_vertecies)).prop_flat_map(move |weights| {
-                    shuffled_subsequence(0..num_vertecies, path_length).prop_map(move |path| (weights.clone(), path))
+                    shuffled_subsequence(0..num_vertecies, path_length)
+                        .prop_map(move |path| (weights.clone(), path))
                 })
             })
         })
@@ -141,14 +147,14 @@ mod test {
     /// Example ballots from https://en.wikipedia.org/wiki/Schulze_method
     fn wiki_ballots() -> Vec<Ballot> {
         let ballots = vec![
-            vec![ vec![(ALICE, 1), (CHAD, 2), (BOB, 3), (ELSA, 4), (DAVE, 5)]; 5],
-            vec![ vec![(ALICE, 1), (DAVE, 2), (ELSA, 3), (CHAD, 4), (BOB, 5)]; 5],
-            vec![ vec![(BOB, 1), (ELSA, 2), (DAVE, 3), (ALICE, 4), (CHAD, 5)]; 8],
-            vec![ vec![(CHAD, 1), (ALICE, 2), (BOB, 3), (ELSA, 4), (DAVE, 5)]; 3],
-            vec![ vec![(CHAD, 1), (ALICE, 2), (ELSA, 3), (BOB, 4), (DAVE, 5)]; 7],
-            vec![ vec![(CHAD, 1), (BOB, 2), (ALICE, 3), (DAVE, 4), (ELSA, 5)]; 2],
-            vec![ vec![(DAVE, 1), (CHAD, 2), (ELSA, 3), (BOB, 4), (ALICE, 5)]; 7],
-            vec![ vec![(ELSA, 1), (BOB, 2), (ALICE, 3), (DAVE, 4), (CHAD, 5)]; 8],
+            vec![vec![(ALICE, 1), (CHAD, 2), (BOB, 3), (ELSA, 4), (DAVE, 5)]; 5],
+            vec![vec![(ALICE, 1), (DAVE, 2), (ELSA, 3), (CHAD, 4), (BOB, 5)]; 5],
+            vec![vec![(BOB, 1), (ELSA, 2), (DAVE, 3), (ALICE, 4), (CHAD, 5)]; 8],
+            vec![vec![(CHAD, 1), (ALICE, 2), (BOB, 3), (ELSA, 4), (DAVE, 5)]; 3],
+            vec![vec![(CHAD, 1), (ALICE, 2), (ELSA, 3), (BOB, 4), (DAVE, 5)]; 7],
+            vec![vec![(CHAD, 1), (BOB, 2), (ALICE, 3), (DAVE, 4), (ELSA, 5)]; 2],
+            vec![vec![(DAVE, 1), (CHAD, 2), (ELSA, 3), (BOB, 4), (ALICE, 5)]; 7],
+            vec![vec![(ELSA, 1), (BOB, 2), (ALICE, 3), (DAVE, 4), (CHAD, 5)]; 8],
         ];
 
         ballots.concat()
@@ -173,58 +179,58 @@ mod test {
         let ballots = wiki_ballots();
         let count = PairwisePreferences::from_ballots(&ballots);
         let widest_paths = floyd_warshall_widest_paths(&count.counts);
-        assert_eq!(widest_paths, vec![
-            vec![i32::MAX, 28, 28, 30, 24],
-            vec![25, i32::MAX, 28, 33, 24],
-            vec![25, 29, i32::MAX, 29, 24],
-            vec![25, 28, 28, i32::MAX, 24],
-            vec![25, 28, 28, 31, i32::MAX],
-        ]);
+        assert_eq!(
+            widest_paths,
+            vec![
+                vec![i32::MAX, 28, 28, 30, 24],
+                vec![25, i32::MAX, 28, 33, 24],
+                vec![25, 29, i32::MAX, 29, 24],
+                vec![25, 28, 28, i32::MAX, 24],
+                vec![25, 28, 28, 31, i32::MAX],
+            ]
+        );
     }
 
     #[test]
     fn simple_floyd_warshall() {
-        let edge_weights = vec![
-            vec![0, 5, 0],
-            vec![0, 0, 5],
-            vec![0, 0, 0],
-        ];
+        let edge_weights = vec![vec![0, 5, 0], vec![0, 0, 5], vec![0, 0, 0]];
         let widest_paths = floyd_warshall_widest_paths(&edge_weights);
-        assert_eq!(widest_paths, vec![
-            vec![i32::MAX, 5, 5],
-            vec![0, i32::MAX, 5],
-            vec![0, 0, i32::MAX],
-        ]);
+        assert_eq!(
+            widest_paths,
+            vec![
+                vec![i32::MAX, 5, 5],
+                vec![0, i32::MAX, 5],
+                vec![0, 0, i32::MAX],
+            ]
+        );
     }
 
     #[test]
     fn simple_floyd_warshall_reversed() {
-        let edge_weights = vec![
-            vec![0, 0, 0],
-            vec![5, 0, 0],
-            vec![1, 5, 0],
-        ];
+        let edge_weights = vec![vec![0, 0, 0], vec![5, 0, 0], vec![1, 5, 0]];
         let widest_paths = floyd_warshall_widest_paths(&edge_weights);
-        assert_eq!(widest_paths, vec![
-            vec![i32::MAX, 0, 0],
-            vec![5, i32::MAX, 0],
-            vec![5, 5, i32::MAX],
-        ]);
+        assert_eq!(
+            widest_paths,
+            vec![
+                vec![i32::MAX, 0, 0],
+                vec![5, i32::MAX, 0],
+                vec![5, 5, i32::MAX],
+            ]
+        );
     }
 
     #[test]
     fn floyd_warshall_chain() {
-        let edge_weights = vec![
-            vec![0, 5, 1],
-            vec![1, 0, 5],
-            vec![3, 1, 0],
-        ];
+        let edge_weights = vec![vec![0, 5, 1], vec![1, 0, 5], vec![3, 1, 0]];
         let widest_paths = floyd_warshall_widest_paths(&edge_weights);
-        assert_eq!(widest_paths, vec![
-            vec![i32::MAX, 5, 5],
-            vec![3, i32::MAX, 5],
-            vec![3, 3, i32::MAX],
-        ]);
+        assert_eq!(
+            widest_paths,
+            vec![
+                vec![i32::MAX, 5, 5],
+                vec![3, i32::MAX, 5],
+                vec![3, 3, i32::MAX],
+            ]
+        );
     }
 
     #[test]
@@ -236,11 +242,14 @@ mod test {
             vec![3, 0, 1, 0],
         ];
         let widest_paths = floyd_warshall_widest_paths(&edge_weights);
-        assert_eq!(widest_paths, vec![
-            vec![i32::MAX, 5, 5, 5],
-            vec![3, i32::MAX, 5, 5],
-            vec![3, 3, i32::MAX, 5],
-            vec![3, 3, 3, i32::MAX],
-        ]);
+        assert_eq!(
+            widest_paths,
+            vec![
+                vec![i32::MAX, 5, 5, 5],
+                vec![3, i32::MAX, 5, 5],
+                vec![3, 3, i32::MAX, 5],
+                vec![3, 3, 3, i32::MAX],
+            ]
+        );
     }
 }
